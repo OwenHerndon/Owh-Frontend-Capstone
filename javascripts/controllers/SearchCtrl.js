c93d7a32c329cc48e08d15ed0b2ab38e06ae0470
Owen <owen.w.herndon@gmail.com>
2016-12-06 20:42:02 -0600

    working woith infowindow listener

--- javascripts/controllers/SearchCtrl.js
+++ javascripts/controllers/SearchCtrl.js
@@ -3,43 +3,65 @@
 app.controller("SearchCtrl", function($scope){
     
     function initAutocomplete() {
-        var map = new google.maps.Map(document.getElementById('map'), {
+        $scope.map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: -33.8688, lng: 151.2195},
           zoom: 13,
           mapTypeId: 'roadmap'
         });
 
+        //geo location
+        var infoWindow = new google.maps.InfoWindow({map: $scope.map});
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            $scope.map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, $scope.map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, $scope.map.getCenter());
+        }
+
+        
         console.log("searchBox", searchBox);
 
         // Create the search box and link it to the UI element.
         var input = document.getElementById('pac-input');
         var searchBox = new google.maps.places.SearchBox(input);
-        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+        $scope.map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
         // Bias the SearchBox results towards current map's viewport.
-        map.addListener('bounds_changed', function() {
-          searchBox.setBounds(map.getBounds());
+        $scope.map.addListener('bounds_changed', function() {
+          searchBox.setBounds($scope.map.getBounds());
         });
 
-        var markers = [];
+        $scope.markers = [];
+
         // Listen for the event fired when the user selects a prediction and retrieve
         // more details for that place.
         searchBox.addListener('places_changed', function() {
-          var places = searchBox.getPlaces();
-
-          if (places.length === 0) {
+          $scope.places = searchBox.getPlaces();
+          console.log("places", $scope.places); //go information to dive into
+          if ($scope.places.length === 0) {
             return;
           }
 
           // Clear out the old markers.
-          markers.forEach(function(marker) {
+          $scope.markers.forEach(function(marker) {
             marker.setMap(null);
           });
-          markers = [];
+          $scope.markers = [];
 
           // For each place, get the icon, name and location.
           var bounds = new google.maps.LatLngBounds();
-          places.forEach(function(place) {
+          $scope.places.forEach(function(place) {
             if (!place.geometry) {
               console.log("Returned place contains no geometry");
               return;
@@ -51,14 +73,34 @@ app.controller("SearchCtrl", function($scope){
               anchor: new google.maps.Point(17, 34),
               scaledSize: new google.maps.Size(25, 25)
             };
-
+       
             // Create a marker for each place.
-            markers.push(new google.maps.Marker({
-              map: map,
+            $scope.markers.push(new google.maps.Marker({
+              map: $scope.map,
               icon: icon,
               title: place.name,
               position: place.geometry.location
             }));
+            console.log("markers", $scope.markers);
+
+            
+
+         //    marker.addListener('click', function() {
+         //  		infoWindow.open($scope.map, $scope.markers);
+         //  		console.log("infoWindow", infoWindow);
+        	// });
+            //taylors button
+            // // for (var i = 0; i < $scope.markers.length; i++) {
+            //    var marker = $scope.markers[i];
+            //     var contentString = '<button class="btn btn-success" ng-click=buttonClick()>Hey</button>';
+            // //    // var compiled = $compile(contentString)($scope);
+            //     google.maps.event.addListener($scope.marker, 'click', function() {
+            //         infoWindow.setContent(contentString);
+            // 	    infoWindow.open($scope.map, this);
+
+            //     });
+           	// // }
+           	//***********************
 
             if (place.geometry.viewport) {
               // Only geocodes have viewport.
@@ -67,8 +109,16 @@ app.controller("SearchCtrl", function($scope){
               bounds.extend(place.geometry.location);
             }
           });
-          map.fitBounds(bounds);
+          $scope.map.fitBounds(bounds);
+          console.log("map scope", $scope.map);
         });
+      }
+
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
       }
 
       initAutocomplete();
